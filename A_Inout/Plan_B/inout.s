86988e214cae0158a0ad54348561ac64b9a50c62
root <root@DESKTOP-95DM1HG.localdomain>
2021-03-05 15:39:15 +0530

    Assignments 1 & 2 complete

--- A_Inout/Plan_B/inout.s
+++ A_Inout/Plan_B/inout.s
@@ -1,54 +1,47 @@
 .text
 assignmentstring: .asciz "Assignment 2: inout\n"
 numberprompt: .asciz "Input a number\n"
-informatstr: .asciz "%d"
-outstr: .asciz "The new number is: %d\n"
+inputstr: .asciz "%d"
+outputstr: .asciz "The new number is: %d\n"
 
 
 .global main
 main:
-  pushq %rbp                      # -8 on stack 
-  movq %rsp, %rbp
+  pushq %rbp                      # initilise base pointer and stack pointer
+  movq %rsp, %rbp                 # initilise base pointer and stack pointer
 
   movq $0, %rax                   # no vector registers in use for printf  
-  movq $assignmentstring, %rdi    # move string to register
-  call printf                     # print names
+  movq $assignmentstring, %rdi    # place $assignmentstring into %rdi
+  call printf                     # output
 
   call inout                      # call function "inout"
 
   end:
-  movq $60, %rax
   mov $0, %rdi
   call exit
 
 
 inout:
    pushq %rbp
-   movq %rsp, %rbp                 # -8
+   movq %rsp, %rbp                 # init
 
    movq $0, %rax                   # no vector registers in use for printf  
-   movq $numberprompt, %rdi        # move string to register
-   call printf                     # print request
-   addq $8, %rsp                   # -8
-
-   subq $8, %rsp                   # -16
-   leaq -16(%rbp), %rsi
-   movq $0, %rax
-   movq $informatstr, %rdi
-   call scanf                      # get input  
-   addq $8, %rsp                   # -16   
-
+   movq $numberprompt, %rdi        # move string to register %rdi
+   call printf                     # output
  
+   leaq -16(%rbp), %rsi            # reserve memory for input
+   movq $0, %rax
+   movq $inputstr, %rdi         
+   call scanf                      # get input  
+   addq $8, %rsp                   # adjust stack pointer for increment below  
 
 
-
-
-    movq -16(%rbp), %rbx
-    incq %rbx
+    movq -16(%rbp), %rbx           
+    incq %rbx                      # increment input value
     movq %rbx, %rsi
 
-    movq $outstr, %rdi
+    movq $outputstr, %rdi
     movq $0, %rax
-    call printf                    # -24                
+    call printf                    # output incremented number               
 
   ret                              # return to main
