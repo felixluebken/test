834a8d48c0a04123b5a98a7027d8f808af93d668
Rafael Pinto <rafasozio15@gmail.com>
2021-03-11 23:24:56 -0300

    recursive working

--- A_recursion/Plan_B/factorial.s
+++ A_recursion/Plan_B/factorial.s
@@ -2,7 +2,7 @@
 assignmentstring: .asciz "Assignment 4: Factorial\n"
 numberprompt: .asciz "Input a number\n"
 inputnmbr: .asciz "%ld"
-outputstr: .asciz "The factorial is: %ld\n"
+outputstr: .asciz "The new number is: %ld\n"
 
 
 .global main
@@ -20,21 +20,25 @@ main:
   movq %rbp, %rsp               # epilogue
   popq %rbp                     #
   
+end:
+  mov  $0, %rdi                 # loads exit code
+  call exit                     # exits the program
+
 inout:
   pushq %rbp                    # Prologue
-  movq  %rsp, %rbp              # Copy stack pointer to RBP 
-  subq  $16, %rsp            
+  movq  %rsp, %rbp              # Copy stack pointer to RBP             
 
   movq $0, %rax                 # no vector registers in use    
   movq $numberprompt, %rdi      # place string into rdi   
   call printf                   # print
- 
+  
+  subq $16, %rsp
   leaq -8(%rbp), %rsi           # loads address of stack var in rsi
   movq $inputnmbr, %rdi         # load argument of scanf
   movq $0, %rax                 # no vector registers in use     
   call scanf                    # call scanf         
    
-  movq -8(%rbp), %rax          # move value at rbp to rax
+  movq -8(%rbp), %rdi           # move value at rbp to rax
   
   call factorial                # call factorial subroutine
 
@@ -49,29 +53,36 @@ inout:
   jmp end                       # jump to end
 
 factorial:
-
-  cmpq $1, -8(%rbp)             # if value is 1 jump to basecase
-  je basecase 
-  cmpq $0, -8(%rbp)             # if value is 0 jump to zerocase
-  je zerocase
-
-  decq -8(%rbp)                 # decrease value by 1
-  mulq -8(%rbp)                 # multiply by the value       
-
-  call factorial                # call factorial again
-
-basecase:
   
-  ret                           # return to inout
+  pushq %rbp                    # Prologue
+  movq  %rsp, %rbp              # Copy stack pointer to RBP  
+
+  pushq %rdi                    # push rdi into the stack
+  movq  -8(%rbp), %rax          # copy the value into rax
+  subq  $8, %rsp
+
+  cmpq  $0, %rax                # compare if value is 0
+  je ifstate                    # jump to ifstate if value is 1
+  jmp elsestate                 # jump to elsestate
+
+ifstate:
+
+  movq $1, %rax                 # move 1 into rax
+  jmp  endstatement             # jump to endstatement
+
+elsestate:
+  
+  movq %rax, -16(%rbp)          # move value in rax 
+  decq %rax                     # decrease value
+  movq %rax, %rdi               # move rax value into rdi
+  call factorial                # call factorial
+  imul -16(%rbp), %rax          # multiply the value by rax and store into rax
+  jmp endstatement              # jump to endstatement 
+
+endstatement:
+
+    movq %rbp, %rsp             # epilogue
+    popq %rbp
+    ret                         # return to inout
                    
-zerocase:
-
-  movq $1, %rax                 # copy 1 into rax
-  jmp  basecase                 # jump to basecase to return  
-
-end:
-  mov  $0, %rdi                 # loads exit code
-  call exit                     # exits the program
-
-
   
